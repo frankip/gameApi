85ec3afedbb0e32ad512d68bec406bd4946bff0c
Francis Kip <gwadafrank@gmail.com>
2019-03-13 13:13:21 +0300

    feat(DB): Create the model class)

--- gamesapi/games/models.py
+++ gamesapi/games/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Game (models.Model):
+    """
+    Game class model that handles all db field
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=200, blank=True, default='')
+    release_date = models.DateTimeField()
+    game_category = models.CharField(max_length=200, blank=True, default='')
+    played = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('name',)
