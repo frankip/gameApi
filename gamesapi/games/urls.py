88430b97dfd4fca904645c764a2f78fe97d2bf28
Francis Kip <gwadafrank@gmail.com>
2019-03-28 01:24:48 +0300

    feat(ApiEndpoints): Implement url to use clases
    - change url from using function based to class based
    - Create a browsable api at the root

--- gamesapi/games/urls.py
+++ gamesapi/games/urls.py
@@ -1,8 +1,26 @@
 from django.conf.urls import url
 from django.urls import path
-from games import views
+from games.views import (
+    GameCategoryList,
+    GameCategoryDetail,
+    GameList,
+    GameDetail,
+    PlayerList,
+    PlayerDetail,
+    PlayerScoreList,
+    PlayerScoreDetail,
+    ApiRoot
+)
 
 urlpatterns = [
-    path('games', views.game_list),
-    path('games/(?P<pk>[0-9]+)/$', views.game_detail)
+    path('games', GameList.as_view(), name=GameList.name),
+    path('games/<int:pk>/', GameDetail.as_view(), name=GameDetail.name),
+    path('game-categories', GameCategoryList.as_view(), name=GameCategoryList.name),
+    path('game-categories/<int:pk>/', GameCategoryDetail.as_view(), name=GameCategoryDetail.name),
+    path('players', PlayerList.as_view(), name=PlayerList.name),
+    path('players/<int:pk>/', PlayerDetail.as_view(), name=PlayerDetail.name),
+    path('player-scores', PlayerScoreList.as_view(), name=PlayerScoreList.name),
+    path('player-scores/<int:pk>/', PlayerScoreDetail.as_view(), name=PlayerScoreDetail.name),
+    path('', ApiRoot.as_view(), name=ApiRoot.name),
+
 ]
--- gamesapi/games/views.py
+++ gamesapi/games/views.py
@@ -47,42 +47,43 @@
 
 """ Class based views"""
 
-from games.model import (
+from games.models import (
     GameCategory,
     Game,
     Player,
     PlayerScore)
 from games.serializers import(
-    GamecategorySerializer,
+    GameCategorySerializer,
     GameSerializer,
     PlayerSerializer,
-    PlayerScoreSeriaizer 
+    PlayerScoreSerializer 
 )
 from rest_framework.response import Response
 from rest_framework.generics import (
     ListCreateAPIView,
-    RetrieveUpdateDestroyAPIView
+    RetrieveUpdateDestroyAPIView,
+    GenericAPIView
 )
 from rest_framework.reverse import reverse
 
 class GameCategoryList(ListCreateAPIView):
-    queryset = GameCategory.objects.all()
-    serializer = GamecategorySerializer
+    queryset = GameCategory.objects.all() 
+    serializer_class = GameCategorySerializer
     name = 'gamecategory-list'
 
 class GameCategoryDetail(RetrieveUpdateDestroyAPIView):
     queryset = GameCategory.objects.all()
-    serializer = GamecategorySerializer
+    serializer_class = GameCategorySerializer
     name = 'gamecategory-detail'
 
 class GameList(ListCreateAPIView):
-    querylist = Game.objects.all()
-    serializer = GamesSerializer
+    queryset = Game.objects.all() 
+    serializer_class = GameSerializer 
     name = 'game-list'
 
 class GameDetail(RetrieveUpdateDestroyAPIView):
     queryset = Game.objects.all()
-    serializer = GameSerializer
+    serializer_class = GameSerializer
     name = 'game-detail'
 
 class PlayerList(ListCreateAPIView): 
@@ -101,9 +102,19 @@ class PlayerScoreList(ListCreateAPIView):
     queryset = PlayerScore.objects.all() 
     serializer_class = PlayerScoreSerializer 
     name = 'playerscore-list' 
- 
- 
+
 class PlayerScoreDetail(RetrieveUpdateDestroyAPIView): 
     queryset = PlayerScore.objects.all() 
     serializer_class = PlayerScoreSerializer 
     name = 'playerscore-detail' 
+
+class ApiRoot(GenericAPIView):
+    name = 'api-root'
+    def get(self, request, *args, **kwargs):
+        return Response({
+            'players': reverse(PlayerList.name, request=request),
+            'game-categories': reverse(GameCategoryList.name, request=request),
+            'games': reverse(GameList.name, request=request),
+            'scores': reverse(PlayerScoreList.name, request=request)
+        })
+        
