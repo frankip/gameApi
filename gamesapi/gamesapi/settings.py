cce4cfb0ff51c2d2fd11816d10d315052771fe79
Francis Kip <gwadafrank@gmail.com>
2019-04-08 01:32:41 +0300

    ft(auth): adding and customizing pagination

--- gamesapi/gamesapi/settings.py
+++ gamesapi/gamesapi/settings.py
@@ -132,3 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'games.pagination.LimitOffsetPaginationWithMaxLimit',
+    'PAGE_SIZE': 3
+}
